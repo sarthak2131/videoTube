c554bf65155e9b08dca2f038ef48d711bd0a40e5
sarthak2131 <sarthakbaderiya19@gmail.com>
2025-01-09 21:20:36 +0530

    changes in some file

--- src/app.js
+++ src/app.js
@@ -23,12 +23,13 @@ app.use(cookieParser())
 
 // import routes
 import healthcheckRouter from './routes/healthcheck.routes.js'
-
+import userRouter from './routes/user.routes.js'
 
 
 // routes
 
 app.use('/api/v1/healthcheck',healthcheckRouter)
+app.use('/api/v1/users',userRouter)
 
 
 export {app};
